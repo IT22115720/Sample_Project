073c887f580046ff4f13c922daa7ec547ae23c83
Sanjana Nimesh <it22102546@my.sliit.lk>
2023-12-07 02:04:02 +0530

    first commit

---
+++ BACKEND/models/student.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+
+    name : {
+
+        type : String,
+        required : true
+
+    },
+
+    age : {
+        type : Number,
+        required : true
+    },
+
+    gender : {
+        type : String,
+        required : true
+    },
+
+    mobile : {
+        type : Number,
+        required : true
+    }
+
+
+});
+
+const student = mongoose.model("Student",studentSchema);
+
+module.exports = student;
---
+++ BACKEND/routes/students.js
@@ -0,0 +1,106 @@
+const router = require("express").Router();
+let Student = require("../models/student")
+
+//insert
+router.route("/add").post((req,res)=>{
+
+    const name = req.body.name; 
+    const age = Number(req.body.age);
+    const gender = req.body.gender;
+    const mobile = Number(req.body.mobile);
+
+    const newStudent = new Student(
+        {
+            name,
+            age,
+            gender,
+            mobile
+        }
+    )
+
+    newStudent.save().then(()=>{
+
+        res.json("Student added");
+ 
+    }).catch((err)=>{
+
+        console.log;
+    });
+
+});
+
+//fetch
+
+router.route("/").get((req,res)=>{
+
+
+    Student.find().then((students)=>
+    {
+        res.json(students)
+    }).catch((err)=>{
+
+        console.log(err);
+    });
+});
+
+//update
+router.route("/update/:sid").put(async(req,res)=>
+{
+    let userId = req.params.sid;
+
+    const {name,age,gender,mobile} = req.body;
+
+    const updateStudent = {
+        name,
+        age,
+        gender,
+        mobile
+    }
+
+    const update = await Student.findByIdAndUpdate(userId,updateStudent).then(()=>{
+
+        res.status(200).send({status:"User updated"});
+
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status:"Error with updating data" , error:err.message});
+    });
+    
+
+})
+
+//delete
+
+router.route("/delete/:sid").delete(async(req,res)=>{
+
+    let userId = req.params.sid;
+
+    await Student.findByIdAndDelete(userId).then(()=>{
+        res.status(200).send({status:"User deleted"});
+    }).catch((err)=>
+    {
+        console.log(err);
+        res.status(500).send({status:"Error with deleting data" , error:err.message});
+    })
+})
+
+//fetch one object
+router.route("/get/:sid").get(async(req,res)=>{
+
+    let userId = req.params.sid;
+
+   const user =  await Student.findById(userId).then((student)=>{
+        res.status(200).send({status:"User fetched",student});
+
+    }).catch((err)=>
+    {
+        console.log(err);
+        res.status(500).send({status:"Error with fetching data" , error:err.message});
+    });
+
+});
+
+
+
+
+module.exports = router;
